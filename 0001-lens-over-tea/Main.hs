1a8de82b8993c4c5590865c0201f85a6b20d4c9d
Jonn Mostovoy <amarr.industrial@gmail.com>
2016-01-05 01:41:22 +0100

    Implement LoT#1

--- 0001-lens-over-tea/Main.hs
+++ 0001-lens-over-tea/Main.hs
@@ -1,5 +1,9 @@
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE TupleSections #-}
+------------------------------------------------------------------------
 module Main where
 import Util
+import Control.Applicative
 ------------------------------------------------------------------------
 type LensF s a = forall f . (Functor f) => (a -> f a) -> s -> (a, f s)
 ixf :: Int -> LensF [a] a
@@ -34,3 +38,43 @@ over l f = runIdentity . l (Identity . f)
 ------------------------------------------------------------------------
 view :: Lens s t a b -> s -> a
 view l = getConst . l Const
+------------------------------------------------------------------------
+
+-- _1 :: Functor f => (a -> f b) -> (a, x) -> f (b, x)
+_1 :: Lens (a, x) (b, x) a b
+_1 g (k, l) = g k >$< (,l)
+
+-- _2 :: Functor f => (a -> f b) -> (x, a) -> f (x, b)
+_2 :: Lens (x, a) (x, b) a b
+_2 g (k, l) = g l >$< (k,)
+
+-- Make a lens out of a getter and a setter.
+lens :: (s -> a) -> (s -> b -> t) -> Lens s t a b
+lens get set g y = (g . get) y >$< set y
+
+-- Combine 2 lenses to make a lens which works on Either. (It's a good idea
+-- to try to use bimap for this, but it won't work, and you have to use
+-- explicit case-matching. Still a good idea, tho.)
+--
+-- :: Functor f1, f2, f3 => ((a -> f1 b) -> s1           -> f1 t1            ) ->
+--                          ((a -> f2 b) -> s2           -> f2 t2            ) ->
+--                           (a -> f3 b) -> Either s1 s2 -> f3 (Either t1 t2)
+choosing :: Lens s1 t1 a b -> Lens s2 t2 a b
+         -> Lens (Either s1 s2) (Either t1 t2) a b
+choosing l1 _  g (Left  y) = l1 g y >$< Left
+choosing _  l2 g (Right y) = l2 g y >$< Right
+
+-- Modify the target of a lens and return the result. (Bonus points if you
+-- do it without lambdas and defining new functions. There's also a hint
+-- before the end of the section, so don't scroll if you don't want it.)
+(<%~) :: Lens s t a b -> (a -> b) -> s -> (b, t)
+(<%~) l h = l (\x -> (h x, h x))
+
+-- Modify the target of a lens, but return the old value.
+(<<%~) :: Lens s t a b -> (a -> b) -> s -> (a, t)
+(<<%~) l h = l (\x -> (x, h x))
+
+-- There's a () in every value. (No idea what this one is for, maybe it'll
+-- become clear later.)
+united :: Lens' s ()
+united g = ($>) $ g ()
--- 0001-lens-over-tea/Util.hs
+++ 0001-lens-over-tea/Util.hs
@@ -1,12 +1,14 @@
 module Util ( ($>),       Compose (..), geqErr, lrgErr, iGuard,
-              Const (..), Product (..), Identity (..)
+              Const (..), Product (..), Identity (..),  first,
+              second,     pamf,         bimap,          (>$<)
             ) where
 
-import           Data.Functor         (($>))
-import           Data.Functor.Compose (Compose (..))
+import           Data.Bifunctor        (first, second, bimap)
+import           Data.Functor          (($>))
+import           Data.Functor.Compose  (Compose (..))
 import           Data.Functor.Identity (runIdentity, Identity(..))
-import           Data.Functor.Product (Product (..))
-import           Control.Applicative  (Const (..))
+import           Data.Functor.Product  (Product (..))
+import           Control.Applicative   (Const (..))
 
 geqErr = error "index must be geq 0"
 lrgErr = error "index too large"
@@ -14,3 +16,6 @@ iGuard x y z f =
   if x < 0
     then geqErr
     else f x y z
+
+pamf  = flip fmap
+(>$<) = pamf
